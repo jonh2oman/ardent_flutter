20733e27075f4c8aa85f47f251009a2f11688364
Jonathan Waterman <jonathanwaterman@Jonathans-MacBook-Pro.local>
2026-05-15 14:32:28 -0230

    Upgraded CorpsData model with element and name fields

--- lib/models/corps_data.dart
+++ lib/models/corps_data.dart
@@ -1,11 +1,14 @@
-
 class CorpsData {
   final String id;
+  final String name;
+  final String element;
   final Map<String, dynamic> settings;
   final Map<String, dynamic> trainingYears;
 
   CorpsData({
     required this.id,
+    required this.name,
+    required this.element,
     required this.settings,
     required this.trainingYears,
   });
@@ -13,6 +16,8 @@ class CorpsData {
   factory CorpsData.fromMap(Map<String, dynamic> data, String id) {
     return CorpsData(
       id: id,
+      name: data['name'] ?? 'Unknown Unit',
+      element: data['element'] ?? 'Sea',
       settings: data['settings'] ?? {},
       trainingYears: data['trainingYears'] ?? {},
     );
@@ -20,6 +25,8 @@ class CorpsData {
 
   Map<String, dynamic> toMap() {
     return {
+      'name': name,
+      'element': element,
       'settings': settings,
       'trainingYears': trainingYears,
     };
